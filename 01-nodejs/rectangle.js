386a1be7916f7c59bdd4151e26612990d0699df8
Piyush Patel <piyushpatel2005@gmail.com>
2017-10-16 23:27:56 +0530

    What is Node Module

---
+++ 01-nodejs/rectangle.js
@@ -0,0 +1,9 @@
+// exports.perimeter = (x, y) => (2 * (x + y));
+// exports.area = (x, y) => (x * y);
+
+var rect = {
+  perimeter : (x, y) => (2 * (x + y)),
+  area : (x, y) => (x * y)
+};
+
+module.exports = rect;
