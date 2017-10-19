54b725bc886fc92fa3a6e168b5f3964b620a7531
Piyush Patel <piyushpatel2005@gmail.com>
2017-10-19 16:51:45 +0530

    Passport authentication using JSON web token

---
+++ 13-passport-jsonwebtokens/models/leaders.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  designation: {
+    type: String,
+    required: true
+  },
+  abbr: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+var Leaders = mongoose.model('Leaders', leaderSchema);
+
+module.exports = Leaders;
